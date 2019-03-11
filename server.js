3b0567c564fd220369b83ce5c4b9e9fef29f486e
B-Tarik <tarik.bouchekkif@gmail.com>
2019-03-11 21:53:09 +0000

    Update server.js

--- server.js
+++ server.js
@@ -2,12 +2,13 @@
 // where your node app starts
 
 // init project
-var express = require('express');
-var app = express();
+const express = require('express');
+const parseDate = require('./helper');
+const app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
-var cors = require('cors');
+const cors = require('cors');
 app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -24,9 +25,13 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+// return date
+app.get("/api/timestamp/:date_string?", (req, res) => {
+  res.json(parseDate(req.params.date_string));
+});
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
